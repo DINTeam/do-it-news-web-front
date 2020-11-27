05e0eedca4969a7409f0684a37c056babed4168a
KangJiJi <kjhmd9726@gmail.com>
2020-11-27 19:38:42 +0900

    [Story] Home page component story 추가

---
+++ pages/index.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Home from "./index";
+
+export default {
+  title: "Example/Home",
+  component: Home,
+};
+
+const Template = () => <Home />;
+
+export const FirstHome = Template.bind({});
