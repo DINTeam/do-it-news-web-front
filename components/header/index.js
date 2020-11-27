5ae41df85f3076e294922d5d78ba9e6c75215273
KangJiJi <kjhmd9726@gmail.com>
2020-11-27 19:38:03 +0900

    [Story] Header component story 추가
    next.js의 Link컴포넌트는 StoryBook에서 사용시 prefetch를 하지 못하기 때문에 꺼줘야함.

--- components/header/index.js
+++ components/header/index.js
@@ -5,7 +5,7 @@ import styles from "./header.module.css";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Link href="/">
+      <Link href="/" prefetch={false}>
         <a className={styles.header__link}>Main</a>
       </Link>
     </div>
