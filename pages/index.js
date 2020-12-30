fa6455ee7015838e0ee69ecbe2b9ac96ed966794
KangJiJi <kjhmd9726@gmail.com>
2020-12-30 14:45:37 +0900

    [Feature] main page 수정
    Button 컴포넌트를 추가함.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Header from '../components/header';
+import Button from '../components/button';
 
 const Home = () => {
   return (
     <div>
       <Header />
       <h1>Hello world!</h1>
+      <Button value="Login" size="large" onClick={() => console.log('hello')} />
     </div>
   );
 };
