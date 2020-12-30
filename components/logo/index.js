a07689dc7cdbec416a78f6dca5a3d821a4378a54
LAPTOP-P9Q4AJ5V\ghtjd <ghtjd626@naver.com>
2020-12-30 16:00:34 +0900

    logo component

---
+++ components/logo/index.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+
+import styles from './logo.module.css';
+
+const getSizeStyle = size => {
+  if (size === 'small') return styles.small;
+  if (size === 'medium') return styles.medium;
+  if (size === 'large') return styles.large;
+
+  return false;
+};
+
+// const getColorStyle = color => {
+//   if (color === 'primary') return styles.primaryColor;
+//   if (color === 'secondary') return styles.secondaryColor;
+//
+//   return false;
+// };
+
+const Logo = props => {
+  const { size } = props;
+
+  const sizeStyle = getSizeStyle(size);
+
+  return (
+    <div>
+      <h className={`${sizeStyle} ${styles.primaryColor}`}>Do</h>
+      <h className={`${sizeStyle} ${styles.primaryColor}`}> it N</h>
+      <h className={`${sizeStyle} ${styles.secondaryColor}`}>ews</h>
+    </div>
+  );
+};
+
+Logo.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  // color: PropTypes.oneOf(['primary', 'secondary']),
+};
+
+Logo.defaultProps = {
+  size: 'medium',
+  // color: 'primary',
+};
+
+export default Logo;
