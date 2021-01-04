7748fc4112bcac0f74607cde1567ba18a02a0953
LAPTOP-P9Q4AJ5V\ghtjd <ghtjd626@naver.com>
2021-01-04 17:11:42 +0900

    [feature] textfield component 수정

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from '../components/header';
 import Logo from '../components/logo';
+import Textfield from '../components/textfield';
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Logo size="medium" />
+      <Textfield active label="아이디" />
     </div>
   );
 };
