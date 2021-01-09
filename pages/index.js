4500b9ff8d99f6fcf65a6f490539dc781e9ed5ce
KangJi <kjhmd9726@gmail.com>
2021-01-09 17:38:06 +0900

    [Feature] Main 페이지에 Card 컴포넌트 적용
    좋아요 싫어요 버튼은 아직 api가 없어서 consle.log로 대체. profile과 news 이미지 또한 이미지 서버가 없어서 temp문자열로 대체.

--- pages/index.js
+++ pages/index.js
@@ -2,41 +2,10 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { getNewsList } from '../api/news';
 import LoadingSpinner from '../components/loadingSpinner';
+import Card from '../components/card';
 
 import useScroll from '../customHooks/useScroll';
 import useLoading from '../customHooks/useLoading';
-
-const CardNews = props => {
-  const { news } = props;
-
-  return (
-    <div>
-      <div>{`Title: ${news.newsName}`}</div>
-      <div>{`Author: ${news.author}`}</div>
-      <div>{`Image: ${news.profileImg}`}</div>
-      <div>{`Date && View: ${news.date}, ${news.views}`}</div>
-      <div>{`Company: ${news.company}`}</div>
-      <div>{`Likes: ${news.likes}`}</div>
-      <div>{`News id: ${news.newsId}`}</div>
-      <div>{`Content: ${news.newsContent}`}</div>
-      <hr />
-    </div>
-  );
-};
-
-CardNews.propTypes = {
-  news: PropTypes.shape({
-    newsId: PropTypes.string.isRequired,
-    newsName: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    views: PropTypes.number.isRequired,
-    company: PropTypes.string.isRequired,
-    profileImg: PropTypes.string.isRequired,
-    likes: PropTypes.number.isRequired,
-    newsContent: PropTypes.string.isRequired,
-  }).isRequired,
-};
 
 const Home = props => {
   const { ssSuccess, ssNewsList, ssPage, ssTotalPage } = props;
@@ -62,12 +31,29 @@ const Home = props => {
     }
   }, [isEnd]);
 
+  const increaseNewsLike = newsId => console.log(`${newsId} Up`);
+
+  const decreaseNewsLike = newsId => console.log(`${newsId} down`);
+
   if (!success) return <div>Error</div>;
   return (
     <>
       {newsList.map(news => (
         // Key는 나중에 newsId로 변경
-        <CardNews key={Math.floor(Math.random() * 1000000)} news={news} />
+        <Card
+          key={Math.floor(Math.random() * 1000000)}
+          onClickIncrease={increaseNewsLike}
+          onClickDecrease={decreaseNewsLike}
+          defaultLike={news.likes}
+          profileImg="temp"
+          author={news.author}
+          title={news.newsName}
+          company={news.company}
+          date={news.date}
+          views={news.views}
+          newsImg="temp"
+          content={news.newsContent}
+        />
       ))}
       {stateNewsListLoading && <LoadingSpinner />}
     </>
