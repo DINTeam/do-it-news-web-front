f7f2fcc8e137496b320f0166f2aeac28f843106e
LAPTOP-P9Q4AJ5V\ghtjd <ghtjd626@naver.com>
2021-01-17 17:47:46 +0900

    [fix] card 컴포넌트에 comment 컴포넌트 추가

--- components/card/contents/index.js
+++ components/card/contents/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Button from '../../button';
 
 import styles from './contents.module.css';
+import CommentList from '../../commentList';
 
 const textLengthOverCut = (txt, img) => {
   let text = txt;
@@ -28,16 +29,38 @@ const Contents = props => {
   const { content, newsImg } = props;
   const shortText = textLengthOverCut(`${content}`, `${newsImg}`);
   const [text, setText] = useState(`${shortText}`);
-  const [value, setValue] = useState('더보기');
+  const [moreValue, setMoreValue] = useState('더보기');
+  const [showComment, setShowComment] = useState(false);
+  const [commentValue, setCommentValue] = useState('댓글보기');
 
-  const onClickBtn = () => {
+  const commentList = [
+    { userName: 'User_A', userId: 'A_Id', comment: 'commentA', date: '2020' },
+    { userName: 'User_B', userId: 'B_Id', comment: 'commentB', date: '2020' },
+    { userName: 'User_C', userId: 'C_Id', comment: 'commentC', date: '2020' },
+    { userName: 'User_A', userId: 'A_Id', comment: 'commentA', date: '2020' },
+    { userName: 'User_B', userId: 'B_Id', comment: 'commentB', date: '2020' },
+    { userName: 'User_C', userId: 'C_Id', comment: 'commentC', date: '2020' },
+  ];
+
+  const moreBtn = () => {
     if (text === content) {
       setText(textLengthOverCut(text, `${newsImg}`));
-      setValue('더보기');
+      setMoreValue('더보기');
     }
     if (!(text === content)) {
       setText(content);
-      setValue('접기');
+      setMoreValue('접기');
+    }
+  };
+
+  const commentBtn = () => {
+    if (commentList.length === 0) setCommentValue('댓글 없음');
+    else if (showComment === true) {
+      setShowComment(false);
+      setCommentValue('댓글 보기');
+    } else {
+      setShowComment(true);
+      setCommentValue('댓글 닫기');
     }
   };
 
@@ -46,13 +69,27 @@ const Contents = props => {
       <div className={`${styles.content}`}>{`${text}`}</div>
       <div className={`${styles.moreBtn}`}>
         <Button
-          value={`${value}`}
+          size="small"
+          value={moreValue}
           color="secondary"
           onClick={() => {
-            onClickBtn();
+            moreBtn();
+          }}
+        />
+        <Button
+          size="small"
+          value={commentValue}
+          color="secondary"
+          onClick={() => {
+            commentBtn();
           }}
         />
       </div>
+      {showComment === true ? (
+        <div className={`${styles.commentList}`}>
+          <CommentList commentList={commentList} />
+        </div>
+      ) : null}
     </div>
   );
 };
--- components/commentList/index.js
+++ components/commentList/index.js
@@ -1,14 +1,28 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Comment from './comment';
 
 import styles from './commentList.module.css';
+import Button from '../button';
 
 const CommentList = props => {
   const { commentList } = props;
+  const [maxItems, setMaxItems] = useState(5);
+  const [moreValue, setMoreValue] = useState('댓글 더보기');
+
+  const moreBtn = () => {
+    if (maxItems > 5) {
+      setMaxItems(5);
+      setMoreValue('댓글 더보기');
+    } else {
+      setMaxItems(commentList.length);
+      setMoreValue('댓글 접기');
+    }
+  };
 
   return (
     <div className={`${styles.commentList} ${commentList}`}>
-      {commentList.map(comment => (
+      {commentList.slice(0, maxItems).map(comment => (
         <Comment
           // key는 commentId로 바꿀 예정
           key={Math.floor(Math.random() * 1000000)}
@@ -18,6 +32,14 @@ const CommentList = props => {
           date={comment.date}
         />
       ))}
+      <div className={`${styles.moreBtn}`}>
+        <Button
+          size="small"
+          value={moreValue}
+          color="secondary"
+          onClick={moreBtn}
+        />
+      </div>
     </div>
   );
 };
