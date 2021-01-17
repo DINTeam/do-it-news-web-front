2769136e38bdc175823aa77e1fe18178569baef2
KangJi <kjhmd9726@gmail.com>
2021-01-18 03:05:37 +0900

    [Fix] signIn 성공시 응답 session storage에 저장하도록 구현

--- helpers/auth/index.js
+++ helpers/auth/index.js
@@ -1,5 +1,6 @@
 import * as authApi from '../../api/auth';
 import * as localStorage from '../localStorage';
+import * as sessionStorage from '../sessionStorage';
 import User from '../../objects/User';
 
 let stateSignIn = false;
@@ -11,23 +12,21 @@ const initState = () => {
   user = new User();
 };
 
-const setState = aUser => {
-  stateSignIn = true;
-  user = aUser;
-};
-
 export const signIn = async (intputUserId, inputUserPw) => {
   // 암호화 필요
   const cryptoPw = inputUserPw;
   const inputUser = new User({ userId: intputUserId, userPw: cryptoPw });
   const response = await authApi.postSignIn(inputUser);
 
-  if (response.signIn) setState();
+  if (response.signIn) {
+    sessionStorage.setItem('user', JSON.stringify({ userId: intputUserId }));
+    sessionStorage.setItem('jwt', response.jwt);
+  }
 
-  return stateSignIn;
+  return response.signIn;
 };
 
-export const keepSignIn = () => localStorage.setItem('jwt', jwtString);
+export const keepSignIn = () => localStorage.setItem('jwt', jwt);
 
 export const signOut = async () => {
   const response = await authApi.postSignIn(user);
