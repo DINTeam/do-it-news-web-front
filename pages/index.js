dc9fe09a70f5f599c867ef4e03b210c155005ed7
KangJi <kjhmd9726@gmail.com>
2021-01-19 01:47:34 +0900

    [Fix] SSR제거

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import { getNewsList } from '../api/news';
 import LoadingSpinner from '../components/loadingSpinner';
 import Card from '../components/card';
@@ -11,29 +10,32 @@ import News from '../objects/News';
 
 const LOADING_SPINNER_HEIGHT = 100;
 
-const Home = props => {
-  const { ssSuccess, ssNewsList, ssPage, ssTotalPage } = props;
-
-  const [success, setSuccess] = useState(ssSuccess);
-  const [newsList, setNewsList] = useState(ssNewsList);
-  const [page, setPage] = useState(ssPage);
-  const [totalPage, setTotalPage] = useState(ssTotalPage);
+const Home = () => {
+  const [success, setSuccess] = useState(true);
+  const [newsList, setNewsList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(2);
 
   const [isEnd] = useScroll(LOADING_SPINNER_HEIGHT);
   const [newsListFetch] = useLoading(getNewsList);
 
-  useEffect(async () => {
-    if (isEnd && page < totalPage) {
-      const res = await newsListFetch(page + 1, 10);
-      if (res.success) {
-        setNewsList([...newsList, ...res.newsList]);
-        setPage(res.page);
-        setTotalPage(res.totalPage);
-      } else {
-        setSuccess(res.success);
-      }
+  const fetchNewsListAndSetState = async () => {
+    const res = await newsListFetch(page + 1, 10);
+    if (res.success) {
+      const newsObjList = res.newsList.map(news => new News(news));
+      setNewsList([...newsList, ...newsObjList]);
+      setPage(res.page);
+      setTotalPage(res.totalPage);
+    } else {
+      setSuccess(res.success);
     }
+  };
+
+  useEffect(() => {
+    if (isEnd && page < totalPage) fetchNewsListAndSetState();
   }, [isEnd]);
+
+  useEffect(fetchNewsListAndSetState, []);
 
   const increaseNewsLike = newsId => console.log(`${newsId} Up`);
 
@@ -44,52 +46,18 @@ const Home = props => {
   return (
     <>
       {newsList.map(aNews => {
-        const news = new News(aNews);
-
         return (
           <Card
             key={Math.floor(Math.random() * 1000000)}
             onClickIncrease={increaseNewsLike}
             onClickDecrease={decreaseNewsLike}
-            news={news}
+            news={aNews}
           />
         );
       })}
       {!(page === totalPage) && <LoadingSpinner />}
     </>
   );
-};
-
-export const getServerSideProps = async () => {
-  const newsListRes = await getNewsList(1, 10).then(res => res);
-
-  return {
-    props: {
-      ssSuccess: newsListRes.success,
-      ssNewsList: newsListRes.newsList,
-      ssPage: newsListRes.page,
-      ssTotalPage: newsListRes.totalPage,
-    },
-  };
-};
-
-Home.propTypes = {
-  ssSuccess: PropTypes.bool.isRequired,
-  ssNewsList: PropTypes.arrayOf(
-    PropTypes.shape({
-      author: PropTypes.string.isRequired,
-      company: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      newsContent: PropTypes.string.isRequired,
-      newsId: PropTypes.string.isRequired,
-      newsName: PropTypes.string.isRequired,
-      views: PropTypes.number.isRequired,
-      profileImg: PropTypes.string.isRequired,
-      likes: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
-  ssPage: PropTypes.number.isRequired,
-  ssTotalPage: PropTypes.number.isRequired,
 };
 
 export default Home;
