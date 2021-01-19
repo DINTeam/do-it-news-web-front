38cc15abbd272abe8ca6af7c753fc157e2d488b1
KangJi <kjhmd9726@gmail.com>
2021-01-20 01:48:56 +0900

    [Fix] loading spinner가 회전하면서 부모 box의 크기를 넘지 않도록 수정

--- components/loadingSpinner/index.js
+++ components/loadingSpinner/index.js
@@ -3,13 +3,15 @@ import styles from './loadingSpinner.module.css';
 const LoadingSpinner = () => {
   return (
     <div className={`${styles.loading}`}>
-      <div className={`${styles.loadingSpinner}`}>
-        <svg className={`${styles.svg}`}>
-          <circle cx="50" cy="12" r="10" className={`${styles.circle}`} />
-          <circle cx="88" cy="50" r="10" className={`${styles.circle}`} />
-          <circle cx="50" cy="88" r="10" className={`${styles.circle}`} />
-          <circle cx="12" cy="50" r="10" className={`${styles.circle}`} />
-        </svg>
+      <div className={`${styles.loadingSpinnerWrapper}`}>
+        <div className={`${styles.loadingSpinner}`}>
+          <svg className={`${styles.svg}`}>
+            <circle cx="50" cy="12" r="10" className={`${styles.circle}`} />
+            <circle cx="88" cy="50" r="10" className={`${styles.circle}`} />
+            <circle cx="50" cy="88" r="10" className={`${styles.circle}`} />
+            <circle cx="12" cy="50" r="10" className={`${styles.circle}`} />
+          </svg>
+        </div>
       </div>
     </div>
   );
