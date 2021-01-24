71b2edc4ea14bf87f965c8bf39a62609eae3914f
KangJi <kjhmd9726@gmail.com>
2021-01-25 00:41:26 +0900

    [Feature] toolbar option 추가

--- components/wysiwygEditor/index.js
+++ components/wysiwygEditor/index.js
@@ -2,19 +2,34 @@
 import { useEffect, useRef } from 'react';
 import 'quill/dist/quill.snow.css';
 
+const TOOLBAR_OPTIONS = [
+  [{ font: [] }],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  ['bold', 'italic', 'underline', 'strike'],
+  [{ color: [] }, { background: [] }],
+  ['blockquote', 'code-block'],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  [{ direction: 'rtl' }],
+  [{ align: [] }],
+  ['link', 'image', 'formula'],
+];
+
+const OPTIONS = {
+  placeholder: '내용을 입력해 주세요.',
+  theme: 'snow',
+  modules: {
+    toolbar: TOOLBAR_OPTIONS,
+  },
+};
+
 const WysiwygEditor = () => {
   const Quill = typeof window === 'object' ? require('quill') : () => false;
 
   const quillElement = useRef();
   const quillInstance = useRef();
 
-  const options = {
-    placeholder: '내용을 입력해 주세요.',
-    theme: 'snow',
-  };
-
   useEffect(() => {
-    quillInstance.current = new Quill(quillElement.current, options);
+    quillInstance.current = new Quill(quillElement.current, OPTIONS);
   }, []);
 
   return (
