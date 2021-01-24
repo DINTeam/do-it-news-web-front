5551344a513e60d77b36447e5297c35a882cf9eb
KangJi <kjhmd9726@gmail.com>
2021-01-24 17:16:24 +0900

    [Feature] quill editor 적용

--- components/wysiwygEditor/index.js
+++ components/wysiwygEditor/index.js
@@ -1,5 +1,27 @@
+/* eslint-disable global-require */
+import { useEffect, useRef } from 'react';
+import 'quill/dist/quill.snow.css';
+
 const WysiwygEditor = () => {
-  return <div>WysiwygEditor</div>;
+  const Quill = typeof window === 'object' ? require('quill') : () => false;
+
+  const quillElement = useRef();
+  const quillInstance = useRef();
+
+  const options = {
+    placeholder: '내용을 입력해 주세요.',
+    theme: 'snow',
+  };
+
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, options);
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <div ref={quillElement} />
+    </div>
+  );
 };
 
 export default WysiwygEditor;
