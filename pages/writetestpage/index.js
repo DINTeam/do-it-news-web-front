dd7ce51ff9922f4c5892769fc839854907a56649
KangJi <kjhmd9726@gmail.com>
2021-01-26 22:32:03 +0900

    [Feature] title input 추가

--- pages/writetestpage/index.js
+++ pages/writetestpage/index.js
@@ -2,15 +2,24 @@ import { useState } from 'react';
 
 import withAuthentication from '../../components/withAuthentication';
 
+import Textfield from '../../components/textfield';
 import WysiwygEditor from '../../components/wysiwygEditor';
 
 const WriteTestPage = () => {
+  const [title, setTitle] = useState('');
   const [contents, setContents] = useState('');
 
+  const handleTitle = event => setTitle(event.target.value);
   const handleContents = value => setContents(value);
 
   return (
     <div>
+      <Textfield
+        value={title}
+        onChange={handleTitle}
+        placeholder="제목을 입력해 주세요"
+        fullWidth
+      />
       <WysiwygEditor
         value={contents}
         onChange={handleContents}
